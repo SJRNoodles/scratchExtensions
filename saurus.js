314ce047cc2c529a22ecc08f1ae0a8339d2ee850
SJRNoodles <54688233+SJRNoodles@users.noreply.github.com>
2021-02-09 21:59:34 -0600

    Update saurus.js

--- saurus.js
+++ saurus.js
@@ -31,7 +31,18 @@ class sau {
                        "defaultValue": "js goes here"
                     },
                }
-            }
+            },
+           {
+            "opcode": "ghtm",
+            "blockType": "reporter",
+            "text": "html request [hmr]",
+           "arguments":{
+              "hmr":{
+                   "type":"string",
+                   "defaultValue":"https://youtube.com"
+                },
+              }
+            },
         ],
         "menus":{
         },
@@ -43,6 +54,26 @@ class sau {
    evr({evrin}){
      
      return(eval(evrin));
+   };
+   ghtm({hmr}){
+      function makeHttpObject() {
+        try {return new XMLHttpRequest();}
+        catch (error) {}
+        try {return new ActiveXObject("Msxml2.XMLHTTP");}
+        catch (error) {}
+        try {return new ActiveXObject("Microsoft.XMLHTTP");}
+        catch (error) {}
+
+        throw new Error("Could not create HTTP request object.");
+      }
+
+      var request = makeHttpObject();
+      request.open("GET", hmr, true);
+      request.send(null);
+      request.onreadystatechange = function() {
+        if (request.readyState == 4)
+          return(request.responseText);
+        };
    };
    eva({evstring}){
       var cmd = evstring;
