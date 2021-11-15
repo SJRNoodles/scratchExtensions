13d280f9668596887869ec2c2ec3f58910fe9bb4
SJRNoodles <54688233+SJRNoodles@users.noreply.github.com>
2021-11-15 10:36:20 -0600

    Update outlet.js

--- outlet.js
+++ outlet.js
@@ -8,7 +8,7 @@ class compy {
            {
             "opcode": "fetchrl",
             "blockType": "reporter",
-            "text": "fetch from url [stringy]",
+            "text": "fetch [stringy]",
            "arguments":{
               "stringy":{
                    "type":"string",
